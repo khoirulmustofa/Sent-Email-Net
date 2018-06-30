47130fede24404da00e0470968e809e14fcfe292
unknown <helpdesk01@helpdesk-PC.grdsteel.com>
2018-06-30 16:32:49 +0700

    Latihan Kirim Bro

---
+++ Email Test/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Email_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKirim = new System.Windows.Forms.Button();
+            this.txtAlamatEmail = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKirim
+            // 
+            this.btnKirim.Location = new System.Drawing.Point(236, 82);
+            this.btnKirim.Name = "btnKirim";
+            this.btnKirim.Size = new System.Drawing.Size(86, 33);
+            this.btnKirim.TabIndex = 0;
+            this.btnKirim.Text = "Kirim";
+            this.btnKirim.UseVisualStyleBackColor = true;
+            this.btnKirim.Click += new System.EventHandler(this.btnKirim_Click);
+            // 
+            // txtAlamatEmail
+            // 
+            this.txtAlamatEmail.Location = new System.Drawing.Point(67, 30);
+            this.txtAlamatEmail.Name = "txtAlamatEmail";
+            this.txtAlamatEmail.Size = new System.Drawing.Size(255, 20);
+            this.txtAlamatEmail.TabIndex = 1;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(67, 121);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(255, 205);
+            this.txtBody.TabIndex = 2;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(67, 56);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(255, 20);
+            this.txtSubject.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Subject";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 342);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtAlamatEmail);
+            this.Controls.Add(this.btnKirim);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKirim;
+        private System.Windows.Forms.TextBox txtAlamatEmail;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Email Test/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace Email_Test
+{
+    public partial class Form1 : Form
+    {
+        string smtpAddress = "smtp.gmail.com";
+        int portNumber = 587;
+        bool enableSSL = true;
+
+        string emailFrom = "khoirul.dev123@gmail.com";
+        string password = "GmailDev@123";
+        //string emailTo = "someone@domain.com";
+        //string subject = "Hello";
+        //string body = "Hello, I'm just writing this to say Hi!";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKirim_Click(object sender, EventArgs e)
+        {
+            string to = txtAlamatEmail.Text;
+            string sub = txtSubject.Text;
+            string body = txtBody.Text;
+            if (String.IsNullOrEmpty(to))
+                return;
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(to);
+                mail.From = new MailAddress(emailFrom);
+                mail.Subject = sub;
+
+                mail.Body = body;
+
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = smtpAddress; //Or Your SMTP Server Address
+                smtp.Credentials = new System.Net.NetworkCredential(emailFrom, password); // ***use valid credentials***
+                smtp.Port = 587;
+
+                //Or your Smtp Email ID and Password
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            //using (MailMessage mail = new MailMessage())
+            //{
+            //    mail.From = new MailAddress(emailFrom);
+
+            //    mail.To.Add(to);
+            //    mail.Subject = subject;
+            //    mail.Body = txtBody.Text;
+            //    mail.IsBodyHtml = true;
+            //    // Can set to false, if you are sending pure text.
+
+            //   // mail.Attachments.Add(new Attachment("C:\\SomeFile.txt"));
+            //   // mail.Attachments.Add(new Attachment("C:\\SomeZip.zip"));
+
+            //    using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+            //    {
+            //        smtp.Credentials = new NetworkCredential(emailFrom, password);
+            //        smtp.EnableSsl = enableSSL;
+            //        smtp.Send(mail);
+            //    }
+            //}
+        }
+    }
+}
